5380e5d230fbe3c2312b6b2de11f10fbbfc1f346
jimin <jimin00study@gmail.com>
2023-12-06 15:01:51 +0900

    code-test브랜치 생성

--- 01-유튜브갤러리/js/script.js
+++ 01-유튜브갤러리/js/script.js
@@ -8,76 +8,66 @@ $(function () {
 
   // 이미지를 저장한 배열을 생성
   const imgArr = [
-    'https://res.heraldm.com/content/image/2023/04/25/20230425000549_0.jpg',
-    'https://image.musinsa.com/mfile_s01/2021/05/13/3fb5ed13afe8714a7e5d13ee506003dd120913.jpg',
-    'https://img.wkorea.com/w/2021/11/style_6189ebb2d3726.jpg',
-    'http://image.yes24.com/images/chyes24/froala/0/48248/58165.jpg',
+    {
+      title: `BLACKPINK - 'How You Like That' M/V`,
+      video: 'https://youtu.be/Pne7e_FGGsM?feature=shared',
+      pic: 'https://res.heraldm.com/content/image/2023/04/25/20230425000549_0.jpg',
+    },
+    {
+      title: '[MV] BIBI(비비) _ BINU(비누)',
+      video: 'https://www.youtube.com/watch?v=6BawyJf4aKs',
+      pic: 'https://image.musinsa.com/mfile_s01/2021/05/13/3fb5ed13afe8714a7e5d13ee506003dd120913.jpg',
+    },
+    {
+      title: `IVE 아이브 'LOVE DIVE' MV`,
+      video: 'https://www.youtube.com/embed/Y8JFxS1HlDo',
+      pic: 'https://img.wkorea.com/w/2021/11/style_6189ebb2d3726.jpg',
+    },
+    {
+      title: `(여자)아이들((G)I-DLE) - 'TOMBOY' Official Music Video`,
+      video: 'https://www.youtube.com/embed/Jh4QFaPmdss',
+      pic: 'http://image.yes24.com/images/chyes24/froala/0/48248/58165.jpg',
+    },
   ];
 
-  // 배열에 접근하는 방법은? : 배열[인덱스]
-  // console.log(imgArr[1]);
-  // body에 배경이미지로 첫번째 이미지를 적용해주세요
-  // $('body').css('background-image', 'url(' + imgArr[0] + ')');
-  // $('body').css('background-image', `url(${imgArr[0]})`);
-  // $selectVideo.css('background-image', `url(${imgArr[0]})`);
+  // 접근 테스트
+  console.log(imgArr[0].pic);
 
-  // console.log(imgArr, $selectVideo);
-
-  // 각 li에 이미지를 각각 뿌리자
-  // 여러개 요소에 각각 어떤 것을 해주려 할 때
-  // 자바스크립트 : forEach(function (item, index) {})
-  // 제이쿼리 : each(function (index, item){})
   $selectVideo.each(function (index, item) {
-    console.log(index, item);
-    // $(item).css('background-image', `url(${imgArr[index]})`);
     setBgImage(item, index);
   });
 
   // $selectVideo를 클릭했을 때
   $selectVideo.on('click', function () {
-    // console.log($(this));
-
-    // 해당 li의 인덱스를 받아오자 videoIdx변수에 담아
     const videoIdx = $(this).index();
-    // imgArr배열에서 인덱스에 해당하는 배경이미지를 body에 적용
-    // $('body').css('background-image', `url(${imgArr[videoIdx]})`);
+
     setBgImage('body', videoIdx);
 
-    // 해당 li의 data-link 값을 가져와서 videolink변수에 담자
-    let videoLink = $(this).attr('data-link');
-    videoLink += '?autoplay=1'; /* videoLink = videoLink + '?autoplay=1'*/
+    // let videoLink = $(this).attr('data-link');
+    let videoLink = imgArr[videoIdx].video;
+    videoLink += '?autoplay=1';
 
-    // 해당 li의 text 값을 가져오자
-    const videoTitle = $(this).text();
-    // console.log(videoLink);
-    // console.log(videoTitle);
-
-    // 그 값을 iframe의 src 값으로 세팅
+    // const videoTitle = $(this).text();
     $video.attr('src', videoLink);
-    // videoTitle을 넣어주기
+
+    const videoTitle = imgArr[videoIdx].title;
     $videoWrap.find('.video-title').text(videoTitle);
 
-    // $videoWrap.find('.video').text($videoText);
-
-    // $dim을 보이게 - fadeIn()
     $dim.fadeIn();
-    // $videoWrap을 보이게 - addClass()
     $videoWrap.addClass('active');
   });
 
   // $btnClose를 클릭했을 때
   $btnClose.on('click', () => {
-    // $dim을 사라지게 - fadeIn()
     $dim.fadeOut();
-    // $videoWrap을 사라지게 - addClass()
+
     $videoWrap.removeClass('active');
-    // 영상정보 초기화, 새로눌러도 기존 내용에서 변경되니까 이전 값 지우기
+
     $video.attr('src', '');
     $videoWrap.find('.video-title').text('');
   });
 
-  // 배경 이미지를 적용하는 공통의 동작을 함수로 정의
   function setBgImage(item, index) {
-    $(item).css('background-image', `url(${imgArr[index]})`);
+    $(item).css('background-image', `url(${imgArr[index].pic})`);
   }
 });
